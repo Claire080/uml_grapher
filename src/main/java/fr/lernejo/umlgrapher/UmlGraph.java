87e9b896a01fff4dcd08d8f91a3367f96f0c0862
enobil franklin claire <enobilfranklin@et.esiea.fr>
2022-10-26 19:43:03 +0200

    Add UmlGraph and GraphType to validate the first tests of UmlGraphTests

---
+++ src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
@@ -0,0 +1,19 @@
+package fr.lernejo.umlgrapher;
+
+public class UmlGraph {
+    private final Class className;
+
+    public UmlGraph(Class className){
+        this.className=className;
+    }
+
+    public String as(GraphType graph_t ){
+        String str="""
+        classDiagram
+        class Machin {
+            <<interface>>
+        }
+        """;
+        return str;
+    }
+}
